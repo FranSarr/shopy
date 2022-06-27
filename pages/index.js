43a22134d413556930377d288729b12a4d5fad1b
fsarrias <fsarrias@kolab.es>
2022-06-27 12:26:59 +0200

    product uploading

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 
+import Heading from 'components/Heading'
+
 export default function Home() {
   return (
     <div>
       <Head>
-        <title></title>
-        <meta name='description' content='' />
+        <title>Digital Downloads</title>
+        <meta name='description' content='Digital Downloads Website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1>Welcome!</h1>
+      
+      <Heading />
+
+      <h1 className='flex justify-center mt-20 text-xl'>Welcome!</h1>
     </div>
   )
 }
